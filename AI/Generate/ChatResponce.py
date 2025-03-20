c71d42ee268ecc311abb39549c49abbdb0a27ee3
DebelToni <anton.ivanov.hristov@gmail.com>
2025-03-20 19:18:37 +0200

    added first version of chat - more stuff to feed needed

--- AI/Generate/ChatResponce.py
+++ AI/Generate/ChatResponce.py
@@ -26,34 +26,40 @@ if __name__ == "__main__":
 
     NPC_data, Offer_Data, Messages = parse()
 
-    Messages["messages"].insert(0, {"role": "OBJECTIVE", "content": ""})
-    Messages["messages"][0]["content"] = "You are an NPC in a videogame about trading."
+    Messages["messages"].insert(0, {"role": "system", "content": ""})
+    Messages["messages"][0]["content"] = "You are an NPC in a videogame about trading and you are negotiating with the player." + " You are selling a " + Offer_Data["item"]["name"] + " to " + NPC_data["name"] + " for"+ Offer_Data["price"] +"."
+    # TODO: actual price
 
-    print(Messages)
+    # print(Messages,"\n-----------")
+    # print(Offer_Data,"\n-----------")
+    # print(NPC_data,"\n-----------")
+
 
     data = {
         "model": "llama3.2:1b",
         # "prompt": question,
-        # "seed": random.randint(0, 10000),
+        "seed": random.randint(0, 10000),
         "messages": [
             # {"role": "user", "content": question}
         ],
-        # "keep_alive": "30m",
+        "keep_alive": "30m",
+        "steal": False,
     }
 
-    print("-------------------1")
+    # print("-------------------1")
     for message in Messages["messages"]:
         data["messages"].append(message)
-    print("-------------------2")
+    # print("-------------------2")
+    # print(data)
 
     # url  = "http://localhost:11434/api/generate"
     url = "http://localhost:11434/api/chat"  # important
 
     response = requests.post(url, json=data)
 
-    print("-------------------3")
-    print(response.text)
-    print("-------------------4")
+    # print("-------------------3")
+    # print(response.text)
+    # print("-------------------4")
 
     answer = ""
     for line in response.text.strip().split("\n"):
@@ -64,7 +70,9 @@ if __name__ == "__main__":
             answer += content
 
     print(answer)
-    print("-------------------")
+    # print("-------------------")
 
 
-# python ChatResponce.py "{\"name\": \"Gosho\"}" "{\"product\": \"banana\"}" "{\"messages\": [ { \"role\": \"user\", \"content\": \"why is the sky blue?\" }, { \"role\": \"assistant\", \"content\": \"due to rayleigh scattering.\" }, { \"role\": \"user\", \"content\": \"how is that different than mie scattering?\" }]}"
+"""
+python ChatResponce.py '{"name": "Gosho"}' '{"item":{"name": "Lada", "price": 1000}}, "description": "Old but gold"}' '{"messages": [ { "role": "user", "contentg": "Can you sell me this for half the price if i give you a old watch" }, { "role": "assistant", "content": "I don't need old watches" }, { "role": "user", "content": "Ok how about getting 10% off for me this time but i contact you again if need more" }]}'
+"""
