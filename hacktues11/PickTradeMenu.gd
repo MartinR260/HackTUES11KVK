715d6a45e992acf2f1416b0e569a7f3c0bd02504
Martin Radnev <martinkata2006@gmail.com>
2025-03-21 01:36:58 +0200

    Added Trade Reroll Animation

--- hacktues11/PickTradeMenu.gd
+++ hacktues11/PickTradeMenu.gd
@@ -1,4 +1,34 @@
 extends Node2D
 
+var button_click_sfx : AudioStreamRandomizer = AudioStreamRandomizer.new()
+var audio_player = AudioStreamPlayer.new()
+@onready var trade_info = $TradeInfo
+@onready var reroll_anim = $TradeInfo/RerollAnimation
+
 func _ready() -> void:
-	pass
+	button_click_sfx.add_stream(0, load("res://sfx/mouseclicks/mouse-button-click-308449.mp3"))
+	button_click_sfx.add_stream(1, load("res://sfx/mouseclicks/mouse-click-sound-233951.mp3"))
+	button_click_sfx.add_stream(2, load("res://sfx/mouseclicks/ui-click-43196.mp3"))
+	button_click_sfx.add_stream(3, load("res://sfx/mouseclicks/ui-click-97915.mp3"))
+	add_child(audio_player) 
+
+func _on_accept_trade_button_up() -> void:
+	audio_player.stream = button_click_sfx.get_stream(randi_range(0, 3))
+	audio_player.play()
+	while audio_player.playing: pass
+	
+	if get_tree().current_scene.name == "ChatTradeMenu":
+		get_tree().change_scene_to_file("res://PickTradeMenu.tscn")
+	else:
+		get_tree().change_scene_to_file("res://ChatTradeMenu.tscn")
+
+
+func _on_reject_trade_button_up() -> void:
+	audio_player.stream = button_click_sfx.get_stream(randi_range(0, 3))
+	audio_player.play()
+	reroll_anim.play("reroll")
+	
+	
+	
+	
+	
