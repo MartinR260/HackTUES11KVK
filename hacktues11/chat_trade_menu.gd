2eb4051919c3efde12ea0e6f8c0e756e5ff77691
Martin Radnev <martinkata2006@gmail.com>
2025-03-22 01:02:33 +0200

    Fixed Animation

--- hacktues11/chat_trade_menu.gd
+++ hacktues11/chat_trade_menu.gd
@@ -2,7 +2,5 @@ extends Node2D
 @onready var ready_anim = $AnimationPlayer
 
 func _ready() -> void:
-	print("starting")
 	ready_anim.play("readyanim")
 	await ready_anim.animation_finished
-	print("ready")
